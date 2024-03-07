44831901919ec6d5e95e8cdbdd1b9e17a5ebe32f
carmentecchia <carmentecchia@gmail.com>
2024-03-07 17:18:51 +0100

    test1

--- src/test/java/com/example/Ex1CrudTest/Ex1CrudTestApplicationTests.java
+++ src/test/java/com/example/Ex1CrudTest/Ex1CrudTestApplicationTests.java
@@ -1,17 +1,27 @@
 package com.example.Ex1CrudTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.assertj.core.api.Assertions.assertThat;
+@AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles(value = "test")
 class Ex1CrudTestApplicationTests {
-@Autowired
-private StudentController studentController;
-@Autowired
-private StudentService studentService;
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+	@Autowired
+	private StudentController studentController;
+	@Autowired
+	private StudentService studentService;
 	@Test
 	void studentControllerLoads() {
 		assertThat(studentController).isNotNull();
